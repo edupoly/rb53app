6272f14c4651e481e9c495fa7f8ed2fd1e7f095a
edupoly <info.edupoly@gmail.com>
2025-08-26 08:00:15 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Banner from "./Banner";
+import { Navbar } from "./Navbar";
+function App() {
+  return (
+    <>
+      <Navbar></Navbar>
+      <Banner></Banner>
+    </>
+  );
+}
+
+export default App;
+// component

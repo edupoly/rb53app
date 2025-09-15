24234ed20d29aa49693f891a6ceeb34374660204
edupoly <info.edupoly@gmail.com>
2025-09-15 09:58:54 +0530

    contextapi with usecontext

--- src/MuniGrandChild.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-function MuniGrandChild() {
-  return (
-    <div className="border border-2 border-primary m-2 p-2">MuniGrandChild</div>
-  );
-}
-
-export default MuniGrandChild;

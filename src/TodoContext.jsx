5d014dd650530aa0e63d2a0c0c97523b9521d0ab
edupoly <info.edupoly@gmail.com>
2025-09-16 07:24:48 +0530

    contextApi-useReducer-todolist

--- src/TodoContext.jsx
+++ src/TodoContext.jsx
@@ -1,8 +1,22 @@
-import { createContext } from "react";
-export intitialState = {
-    todos:[
-        {
-            
-        }
-    ]
+import { createContext, useReducer } from "react";
+import { alltodos } from "./todos";
+export const intitialState = {
+  todos: [...alltodos],
+};
+function todoReducer(state, action) {
+  switch (action.type) {
+    case "ADDTODO":
+      return { ...state, todos: [...state.todos, action.payload] };
+  }
 }
+
+export const TodoContext = createContext();
+
+export const TodoProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(todoReducer, intitialState);
+  return (
+    <TodoContext.Provider value={{ state, dispatch }}>
+      {children}
+    </TodoContext.Provider>
+  );
+};

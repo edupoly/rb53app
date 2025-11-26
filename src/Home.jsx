cb9080caeae6b1de84009c97c710f03ffb9fb4e0
edupoly <info.edupoly@gmail.com>
2025-11-26 18:14:13 +0530

    pror

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,6 +1,12 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 function Home() {
+  useEffect(() => {
+    console.log("Home Gadu puttadu");
+    return () => {
+      console.log("Home gadu Chachadu");
+    };
+  }, []);
   return (
     <div>
       <h1>Home Page Component</h1>
